f612e7288f42121d1d2db109fa151afdf456acc2
brev horton <brev_horton@outlook.com>
2023-08-31 15:48:52 -0400

    PE1 - Hello World finished homework

---
+++ Horton_HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horton_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brevin Leo Horton");
+
+            int intOne = 3;
+            double doubleOne = 2.5;
+
+            Console.WriteLine(intOne*doubleOne);
+
+            for(int i = 0; i < intOne; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
