f738ac312f0fdb530dbd95a4bc97873f71ee868d
brevcane <blh6705@g.rit.edu>
2023-09-19 18:27:05 -0400

    Double Quotes PE8

---
+++ doubleQuotes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubleQuotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt the user for a string
+            Console.WriteLine("Input a string: ");
+
+            // store the string into a variable
+            string input = Console.ReadLine();
+
+            // create an empty string variable
+            string newInput = "";
+
+            string[] words = input.Split();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                newInput += '"' + words[i] + '"' + ' ';
+            }
+
+            Console.WriteLine("New string: " + newInput);
+        }
+    }
+}
