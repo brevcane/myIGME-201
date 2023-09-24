a2de5023cf92c7ad8e045cb5be673b812bb802a7
brevcane <blh6705@g.rit.edu>
2023-09-24 14:13:16 -0400

    delegate function that mimics read line

---
+++ readLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readLine
+{
+    internal class Program
+    {
+        delegate string readDelegate();
+        static string readLine()
+        {
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            readDelegate ReadLine = readLine;
+        }
+    }
+}
