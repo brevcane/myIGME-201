72c7c8af698547a0b49d6e02c970a94443a26fe2
brevcane <blh6705@g.rit.edu>
2023-09-30 14:18:22 -0400

    Salary

---
+++ salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring variables
+            string sName;
+            double dSalary = 30000;
+            Console.WriteLine("Enter your name: ");
+            sName = Console.ReadLine();
+            bool raise = GiveRaise(sName, ref dSalary);
+
+            if(raise == true)
+            {
+                Console.WriteLine("Congrats on the raise! Your salary is now " + dSalary);
+            }
+
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name == "Brevin")
+            {
+                salary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ test4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            byte byteVal;
+            short shortVal = -556;
+            byteVal = Convert.ToByte(shortVal);
+            Console.WriteLine("byteVal = {0}", byteVal);
+
+
+
+
+        }
+    }
+}
