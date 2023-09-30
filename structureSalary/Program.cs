4be84b067f5d1c4dc28fb4aad43e4a848b94ac1c
brevcane <blh6705@g.rit.edu>
2023-09-30 14:24:02 -0400

    structureSalary

---
+++ structureSalary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structureSalary
+{
+    internal class Program
+    {
+        struct Employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+        static void Main(string[] args)
+        {
+            // declaring variables
+            Employee e = new Employee();
+            e.dSalary = 30000;
+
+            Console.WriteLine("Enter your name: ");
+            e.sName = Console.ReadLine();
+
+            bool raise = GiveRaise(ref e);
+
+            if (raise == true)
+            {
+                Console.WriteLine("Congrats on the raise! Your salary is now " + e.dSalary);
+            }
+
+        }
+
+        static bool GiveRaise(ref Employee employee)
+        {
+            if (employee.sName == "Brevin")
+            {
+                employee.dSalary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
