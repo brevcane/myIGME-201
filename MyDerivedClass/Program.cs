3c31f007035702f9f9266eb861bc6261f42461b8
brevcane <blh6705@g.rit.edu>
2023-10-12 17:25:48 -0400

    PE12 derived class

---
+++ MyDerivedClass/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDerivedClass
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            private string myString;
+
+            public MyClass(string input)
+            {
+                myString = input;
+            }
+
+            public string MyString
+            {
+                set
+                {
+                    myString = value;
+                }
+            }
+
+
+            public virtual string GetString()
+            {
+                return myString;
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public MyDerivedClass(string input) : base(input) { }
+
+            public override string GetString()
+            {
+                string derivedString = base.GetString();
+                return derivedString + " (output from the derived class)";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerivedClass derivedClass = new MyDerivedClass("MyClass string");
+            Console.WriteLine(derivedClass.GetString());
+        }
+    }
+}
