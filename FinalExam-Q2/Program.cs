9759e4affd8f7f8ec666987366143284905ff95f
brevcane <blh6705@g.rit.edu>
2023-12-15 18:45:17 -0500

    Final Exam question 2

---
+++ FinalExam-Q2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Q2
+{
+    internal class Program
+    {
+        class MyQueue
+        {
+            private List<int> myQueue = new List<int>();
+
+            public void Enqueue(int n)
+            {
+                myQueue.Add(n);
+            }
+
+            public int Dequeue()
+            {
+                int dequeued = myQueue[0];
+                myQueue.RemoveAt(0);
+                return dequeued;
+            }
+
+            public int Peek()
+            {
+                return myQueue[0];
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
