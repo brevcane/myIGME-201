fe2265b4af82ca96b3df187c3921ee0db1f2bd3b
brevcane <blh6705@g.rit.edu>
2023-12-15 16:37:18 -0500

    Final Exam question 1

---
+++ FinalExam-Q1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Q1
+{
+    internal class Program
+    {
+        class MyStack
+        {
+            private List<int> myStack = new List<int>();
+
+            public void Push(int n)
+            {
+                myStack.Add(n);
+            }
+
+            public int Pop()
+            {
+                int removed = myStack[myStack.Count - 1];
+                myStack.RemoveAt(myStack.Count - 1);
+                return removed;
+            }
+
+            public int Peek()
+            {
+                return myStack[myStack.Count - 1];
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
